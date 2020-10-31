759f2e7e82d809fa676697ec49e5e3a20a47b473
GamingLiamStudios <swimmingliam@gmail.com>
2020-10-31 18:02:46 +1100

    Actually reverted stuff properly this time

--- source/kernel/entry.asm
+++ source/kernel/entry.asm
@@ -1,26 +1,19 @@
 [bits 32]
-[extern _kernel]
 _detect_64:
-    ; Clear VGA Buffer
-    mov eax, 0x0f200f20
-    mov ecx, 1000
-    mov edi, 0xb8000
-    cld
-    rep stosd
-
     ; Detect CPUID
     pushfd
     pop eax
-    mov ecx, eax
+
+    mov eax, ecx
     xor eax, 1 << 21
     push eax
     popfd
 
     pushfd
     pop eax
+
     push ecx
     popfd
-
     xor eax, ecx
     jnz .detect_lm ; Detect long mode if CPUID is supported
 
@@ -32,20 +25,14 @@ _detect_64:
     rep movsw
     hlt
 
-    .detect_lm; Detect Long Mode support
-    mov eax, 0x80000000
-    cpuid
-    cmp eax, 0x80000001
-    jb .no_lm
-
+    .detect_lm:
     mov eax, 0x80000001
     cpuid
     test edx, 1 << 29
     jnz _enter_lm
 
-    .no_lm:
     ; Print error message
-    mov esi, lm_err
+    mov esi, cpuid_err
     mov cx, 19
     mov edi, 0xb8000
     cld
@@ -55,17 +42,15 @@ _detect_64:
 _enter_lm:
     call id_paging_setup
 
-<<<<<<< HEAD
-=======
     ; Edit GDT for 64-bit usage
     mov byte [gdt_codedesc + 6], 10101111b
     mov byte [gdt_datadesc + 6], 10101111b
 
->>>>>>> parent of b6d9217... 64-bit GDT, bootloop tho
     jmp codeseg:enter_kernel
 
     jmp $
 
+%include "source/boot/gdt.asm"
 %include "source/kernel/id_paging.asm"
 
 cpuid_err: ; 'CPUID not Supported'
@@ -75,7 +60,9 @@ lm_err: ; 64bit not Supported
     dw 0x0f36, 0x0f34, 0x0f62, 0x0f69, 0x0f74, 0x0f20, 0x0f6e, 0x0f6f, 0x0f74, 0x0f20, 0x0f53, 0x0f75, 0x0f70, 0x0f70, 0x0f6f, 0x0f72, 0x0f74, 0x0f65, 0x0f64
  
 [bits 64]
+[extern kernel]
 
 enter_kernel:
-    call _kernel
+    call kernel
+
     jmp $
