ee745a5fea5d79ef26fee947fd508cc8b2b7d6e7
GamingLiamStudios <swimmingliam@gmail.com>
2020-11-01 01:24:15 +1100

    Removed some debugging stuff, printf broken

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -2,8 +2,5 @@
 
 void _kernel() {
     clear();
-    printf("Hello World!\n");
-    printf(
-        "Isn't it lovely to have printf working?\nIts nice for me, at "
-        "least.\n");
+    printc('x');
 }
