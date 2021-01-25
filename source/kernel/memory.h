81c1e7b5fd7ecc1e906675dd39b0fad0b6f5f59c
GamingLiamStudios <swimmingliam@gmail.com>
2021-01-25 22:59:34 +1100

    Another benchmark for memcpy

--- source/kernel/memory.h
+++ source/kernel/memory.h
@@ -1,7 +1,9 @@
 #pragma once
 #ifndef MEMH
 #define MEMH
+
 // https://quick-bench.com/q/A9nIt8KHyL3h5TkHIWSvjjJbMes
+// https://quick-bench.com/q/vKWM8x5rqo2rCdzCfa73Al59F3k
 void memcpy(void *dest, void *src, int n) {
     int mod = n % 4;
     __asm__ __volatile__("cld; rep movsd;"
@@ -9,4 +11,5 @@ void memcpy(void *dest, void *src, int n) {
                          : "c"((n - mod) / 4), "S"(src), "D"(dest));
     while (mod--) *((char *)dest + n - mod - 1) = *((char *)src + n - mod - 1);
 }
+
 #endif
